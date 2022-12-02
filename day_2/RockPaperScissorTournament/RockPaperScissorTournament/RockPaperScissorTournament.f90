ee481e1b94b4863dc22ac289453a5e5310bd8b4c
Ralf Burghardt <ralf.burghardt@imab.tu-clausthal.de>
2022-12-02 08:38:59 +0100

    Day 2 of Advent of Code

---
+++ day_2/RockPaperScissorTournament/RockPaperScissorTournament/RockPaperScissorTournament.f90
@@ -0,0 +1,106 @@
+
+program RockPaperScissorTournament
+use ISO_FORTRAN_ENV
+implicit none
+
+! Variables
+character(1) :: opponent
+character(1) :: me
+integer	     :: score_sum
+integer	     :: io_status
+
+! PreCalculation (Part 1)
+open(file='strategyguide.txt', unit=42)
+
+read(42,*,iostat=io_status) opponent, me
+score_sum  = 0
+
+do while(io_status == 0)
+		score_sum = score_sum + calculateScore(opponent,me)
+		read(42,*,iostat=io_status) opponent, me
+end do
+
+print *, score_sum
+read(*,*)
+
+print *, '--------------'
+! PostCalculation (Part 2)
+rewind(42)
+read(42,*,iostat=io_status) opponent, me
+score_sum  = 0
+
+do while(io_status == 0)
+		score_sum = score_sum + calculateScore_new(opponent,me)
+		read(42,*,iostat=io_status) opponent, me
+end do
+
+print *, score_sum
+read(*,*)
+
+contains
+	function calculateScore(opponent_shape, my_shape) result(score)
+		character(1),intent(in) :: opponent_shape
+		character(1),intent(in) :: my_shape
+		integer					:: score
+		
+		if (opponent_shape == 'A') then      ! opponent chooses rock
+			if (my_shape == 'X') then
+				score = 3 + 1 
+			elseif (my_shape == 'Y') then
+				score = 6 + 2
+			elseif (my_shape == 'Z') then
+				score = 0 + 3
+			end if		
+		elseif (opponent_shape == 'B' ) then ! opponent chooses paper
+			if (my_shape == 'X') then
+				score = 0 + 1
+			elseif (my_shape == 'Y') then
+				score = 3 + 2
+			elseif (my_shape == 'Z') then
+				score = 6 + 3
+			end if	
+		elseif (opponent_shape == 'C') then  ! opponent chooses scissor
+			if (my_shape == 'X') then
+				score = 6 + 1
+			elseif (my_shape == 'Y') then
+				score = 0 + 2
+			elseif (my_shape == 'Z') then
+				score = 3 + 3
+			end if	
+		end if
+	end function calculateScore
+	
+	function calculateScore_new(opponent_shape, intended_result) result(score)
+		character(1),intent(in) :: opponent_shape
+		character(1),intent(in) :: intended_result
+		integer					:: score
+		
+		if (opponent_shape == 'A') then	    ! opponent chooses rock
+			if (intended_result == 'X') then
+				score = 0 + 3				! 0 for loosing and 3 for scissor 
+			elseif (intended_result == 'Y') then
+				score = 3 + 1				! 3 for draw and 1 for rock 
+			elseif (intended_result == 'Z') then
+				score = 6 + 2				! 6 for win and 2 for paper 
+			end if		
+		elseif (opponent_shape == 'B') then ! opponent chooses paper
+			if (intended_result == 'X') then
+				score = 0 + 1			    ! 0 for loosing and 1 for rock 
+			elseif (intended_result == 'Y') then
+				score = 3 + 2               ! 3 for draw and 2 for paper 
+			elseif (intended_result == 'Z') then
+				score = 6 + 3               ! 6 for win and 3 for scissor 
+			end if	
+		elseif (opponent_shape == 'C') then ! opponent chooses scissor
+			if (intended_result == 'X') then
+				score = 0 + 2               ! 0 for loosing and 2 for paper 
+			elseif (intended_result == 'Y') then
+				score = 3 + 3				! 3 for draw and 3 for scissor 
+			elseif (intended_result == 'Z') then
+				score = 6 + 1               ! 6 for win and 1 for rock 
+			end if	
+		end if
+	end function calculateScore_new
+
+end program RockPaperScissorTournament
+
